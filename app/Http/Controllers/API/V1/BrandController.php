b76d64df248538b0bb055d7a1a60af465f12c927
longka <nguyentienlong88@gmail.com>
2020-08-02 21:51:54 +0700

    Add brand controller

---
+++ app/Http/Controllers/API/V1/BrandController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers\API\V1;
+
+use App\Models\Brand;
+use Illuminate\Http\Request;
+
+class BrandController extends BaseController
+{
+    protected $brand;
+
+    public function __construct(Brand $brand)
+    {
+        $this->brand = $brand;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function list()
+    {
+        $brands = $this->brand->all();
+        
+        return $this->sendResponse($brands,'Brand list');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Http/Requests/Campaigns/CampaignRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests\Campaigns;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CampaignRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|min:5',
+            'contract_code' => 'nullable|string',
+            'license_code' => 'nullable|string',
+            'brand_id' => 'required|integer',
+            'position_list' => 'required|array',
+            'from_ts' => 'required|integer',
+            'to_ts' => 'required|integer',
+            'discount_type' => 'required|string',
+            'discount_value' => ['required','regex:/^\d+\.\d+|\d+$/'],
+            'discount_max' => 'required|regex:/^\d*(\.\d{2})?$/',
+            'total_discount' => 'required|regex:/^\d*(\.\d{2})?$/',
+            'total_price' => 'required|regex:/^\d*(\.\d{2})?$/',
+        ];
+    }
+}
