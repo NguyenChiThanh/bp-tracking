b8765146883ab1788ac78292a9c881612176dab5
Long Nguyen <nguyentienlong88@gmail.com>
2020-09-05 12:51:20 +0700

    Update prod.sh script

--- prod.sh
+++ prod.sh
@@ -1,16 +1,44 @@
 #!/usr/bin/env sh
-cp .env.prod .env
-composer install
-composer du
-php artisan cache:clear
+function init {
+    cp .env.prod .env
+    composer install
+    composer du
+    php artisan cache:clear
 
-php artisan mig:fre
-php artisan sync:location
-php artisan sync:store
-php artisan sync:brand
+    php artisan mig:fre
+    php artisan sync:location
+    php artisan sync:store
+    php artisan sync:brand
 
-# php artisan db:seed
-php artisan impo:posi positions_prod.xlsx
+    # php artisan db:seed
+    php artisan import:pos positions_prod.xlsx
 
-npm install
-npm run build
+    npm install
+    npm run prod
+}
+
+function deploy {
+    cp .env.prod .env
+    composer install
+    composer du
+    php artisan cache:clear
+
+    php artisan sync:location
+    php artisan sync:store
+    php artisan sync:brand
+
+    php artisan import:pos positions_prod.xlsx
+
+    npm install
+    npm run prod
+}
+
+ACTION=$1
+
+if [[ "$ACTION" == 'init' ]]; then
+    echo "initializing prod ..."
+    init
+else
+    echo "deploying prod ..."
+    deploy
+fi
